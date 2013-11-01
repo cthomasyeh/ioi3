ff207ba675e7c829bcdfaa41c46d9f52fac0910a
cthomasyeh <tyeh@broadwayedu.com>
2013-10-31 18:01:43 -0700

    commit

--- structsort.cpp
+++ structsort.cpp
@@ -27,8 +27,6 @@ int main()
 		ifs >> v[i].firstName >> v[i].lastName >> v[i].age >> v[i].gpa;
 	}
 	
-	for (iter=v.begin(); iter!=v.end(); iter++)
-		cout << (*iter).firstName << endl;
 
 	for (int i=0; i<v.size(); i++)
 		cout << v[i].firstName << endl;
@@ -37,12 +35,7 @@ int main()
 
 	cout << sp->lastName << endl;;
 	cout << (*sp).gpa << endl;
-
-	int a= 100;
-	int * b = &a;
-	cout << *b << endl;
-	cout << &a << endl;
-
+	
 	system("pause");
 
 }
